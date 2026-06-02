4214b2f3fcf3c69f30afae6841e38290653d6c46
Juan Caldera <84481972+Calderovsky@users.noreply.github.com>
2026-06-01 21:52:38 -0600

    Se consideran arreglos de enteros de cualquier tamaño. Se soluciono el problema de completar el arreglo cuando se llegaba al final de cualquiera de los dos subarreglos.

--- mergesort/TestMergeSort.java
+++ mergesort/TestMergeSort.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 class MergeSort {
@@ -12,8 +13,8 @@ class MergeSort {
             return numeros;
         }
 
-        lh = Arrays.copyOfRange(numeros, 0, tamano / 2);
-        rh = Arrays.copyOfRange(numeros, tamano / 2, tamano);
+        lh = Arrays.copyOfRange(numeros, 0, tamano/2);
+        rh = Arrays.copyOfRange(numeros, tamano/2, tamano);
 
         lho = mergesort(lh);
         rho = mergesort(rh);
@@ -23,27 +24,28 @@ class MergeSort {
     }
 
     public static int[] merge(int[] lh, int[] rh) {
-        int[] result = new int[2 * lh.length]; 
-        int i = 0;
-        int j = 0;
+        int n = lh.length + rh.length;
+        int[] result = new int[n]; 
+        int li = 0;
+        int ri = 0;
 
-        for(int k = 0; k < 2 * lh.length; k++) {
-            if(i == lh.length) {
-                result[k] = rh[j];
+        for(int k = 0; k < n; k++) {
+            if(li == lh.length) {
+                System.arraycopy(rh, ri, result, k, rh.length - ri); 
                 break;
             }
 
-            if(j == rh.length) {
-                result[k] = lh[i];
+            if(ri == rh.length) {
+                System.arraycopy(lh, li, result, k, lh.length - li);
                 break;
             }
 
-            if(lh[i] < rh[j]) {
-                result[k] = lh[i];
-                i = i + 1;
+            if(lh[li] < rh[ri]) {
+                result[k] = lh[li];
+                li = li + 1;
             } else {
-                result[k] = rh[j];
-                j = j + 1;
+                result[k] = rh[ri];
+                ri = ri + 1;
             }
         }
 
@@ -53,7 +55,7 @@ class MergeSort {
 
 public class TestMergeSort {
     public static void main(String[] args) {
-        int[] numeros = {1, 1, 1, 1};
+            int[] numeros = {10, 5, 4, 2, 1, 8, 7, 2, 9, 6, 3};
         System.out.println(Arrays.toString(MergeSort.mergesort(numeros)));
     }
 }
